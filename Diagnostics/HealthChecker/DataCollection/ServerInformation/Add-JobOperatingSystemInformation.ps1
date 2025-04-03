ebed93749e068cdbd8ac4fd9bfeed593021bfd1b
David Paulson <dpaul@microsoft.com>
2025-04-03 16:47:04 -0500

    Update Add-JobOperatingSystemInformation squash

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Add-JobOperatingSystemInformation.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Add-JobOperatingSystemInformation.ps1
@@ -8,7 +8,11 @@ function Add-JobOperatingSystemInformation {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [string]$ComputerName
+        [string]$ComputerName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("Legacy", "Queue", "StartNow")]
+        [string]$RunType
     )
     process {
         <#
@@ -29,21 +33,27 @@ function Add-JobOperatingSystemInformation {
         . $PSScriptRoot\Invoke-JobOperatingSystemInformation.ps1
 
         Write-Verbose "Calling: $($MyInvocation.MyCommand)"
-        $sbInjectionParams = @{
-            PrimaryScriptBlock = ${Function:Invoke-JobOperatingSystemInformation}
-            IncludeScriptBlock = @(${Function:Get-WmiObjectCriticalHandler}, ${Function:Get-WmiObjectHandler}, ${Function:Get-RemoteRegistryValue},
-                ${Function:Get-RemoteRegistrySubKey}, ${Function:Get-LocalizedCounterSamples}, ${Function:Get-CounterSamples},
-                ${Function:Get-LocalizedPerformanceCounterName}, ${Function:Get-CounterFullNameToCounterObject}, ${Function:Get-VisualCRedistributableInstalledVersion},
-                ${Function:Get-NETFrameworkVersion}, ${Function:GetNetVersionDictionary}, ${Function:ValidateNetNameParameter})
-        }
-        $scriptBlock = Get-HCDefaultSBInjection @sbInjectionParams
-        $params = @{
-            JobParameter = @{
-                ComputerName = $ComputerName
-                ScriptBlock  = $scriptBlock
+
+        if ($RunType -eq "Legacy") {
+            throw "Legacy Not Implemented"
+        } else {
+            $sbInjectionParams = @{
+                PrimaryScriptBlock = ${Function:Invoke-JobOperatingSystemInformation}
+                IncludeScriptBlock = @(${Function:Get-WmiObjectCriticalHandler}, ${Function:Get-WmiObjectHandler}, ${Function:Get-RemoteRegistryValue},
+                    ${Function:Get-RemoteRegistrySubKey}, ${Function:Get-LocalizedCounterSamples}, ${Function:Get-CounterSamples},
+                    ${Function:Get-LocalizedPerformanceCounterName}, ${Function:Get-CounterFullNameToCounterObject}, ${Function:Get-VisualCRedistributableInstalledVersion},
+                    ${Function:Get-NETFrameworkVersion}, ${Function:GetNetVersionDictionary}, ${Function:ValidateNetNameParameter})
             }
-            JobId        = "Invoke-JobOperatingSystemInformation-$ComputerName"
+            $scriptBlock = Get-HCDefaultSBInjection @sbInjectionParams
+            $params = @{
+                JobParameter = @{
+                    ComputerName = $ComputerName
+                    ScriptBlock  = $scriptBlock
+                }
+                JobId        = "Invoke-JobOperatingSystemInformation-$ComputerName"
+                TryStartNow  = $RunType -eq "StartNow"
+            }
+            Add-JobQueue @params
         }
-        Add-JobQueue @params
     }
 }
