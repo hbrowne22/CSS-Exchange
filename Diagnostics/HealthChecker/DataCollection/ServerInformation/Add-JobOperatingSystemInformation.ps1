5ac0860f0822e656cb563ee86224746cf96eeca6
David Paulson <dpaul@microsoft.com>
2025-04-24 15:50:17 -0500

    Update Add-JobOperatingSystemInformation squash

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Add-JobOperatingSystemInformation.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Add-JobOperatingSystemInformation.ps1
@@ -15,34 +15,31 @@ function Add-JobOperatingSystemInformation {
         [string]$RunType
     )
     process {
-        <#
-            Non Default Script Block Dependencies
-                Get-WmiObjectCriticalHandler
-                Get-WmiObjectHandler
-                Get-RemoteRegistryValue
-                Get-RemoteRegistrySubKey
-                Get-LocalizedCounterSamples
-                Get-CounterSamples
-                Get-LocalizedPerformanceCounterName
-                Get-CounterFullNameToCounterObject
-                Get-VisualCRedistributableInstalledVersion
-                Get-NETFrameworkVersion
-                GetNetVersionDictionary
-                ValidateNetNameParameter
-        #>
+
         . $PSScriptRoot\Invoke-JobOperatingSystemInformation.ps1
 
         Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+        $nonDefaultSbDependencies = @(
+            ${Function:GetNetVersionDictionary},
+            ${Function:ValidateNetNameParameter},
+            ${Function:Get-CounterFullNameToCounterObject},
+            ${Function:Get-CounterSamples},
+            ${Function:Get-LocalizedCounterSamples},
+            ${Function:Get-LocalizedPerformanceCounterName},
+            ${Function:Get-NETFrameworkVersion},
+            ${Function:Get-RemoteRegistrySubKey},
+            ${Function:Get-RemoteRegistryValue},
+            ${Function:Get-VisualCRedistributableInstalledVersion}
+            ${Function:Get-WmiObjectCriticalHandler},
+            ${Function:Get-WmiObjectHandler}
+        )
 
         if ($RunType -eq "Legacy") {
             throw "Legacy Not Implemented"
         } else {
             $sbInjectionParams = @{
                 PrimaryScriptBlock = ${Function:Invoke-JobOperatingSystemInformation}
-                IncludeScriptBlock = @(${Function:Get-WmiObjectCriticalHandler}, ${Function:Get-WmiObjectHandler}, ${Function:Get-RemoteRegistryValue},
-                    ${Function:Get-RemoteRegistrySubKey}, ${Function:Get-LocalizedCounterSamples}, ${Function:Get-CounterSamples},
-                    ${Function:Get-LocalizedPerformanceCounterName}, ${Function:Get-CounterFullNameToCounterObject}, ${Function:Get-VisualCRedistributableInstalledVersion},
-                    ${Function:Get-NETFrameworkVersion}, ${Function:GetNetVersionDictionary}, ${Function:ValidateNetNameParameter})
+                IncludeScriptBlock = $nonDefaultSbDependencies
             }
             $scriptBlock = Get-HCDefaultSBInjection @sbInjectionParams
             $params = @{
